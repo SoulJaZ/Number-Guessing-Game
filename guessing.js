e14cf740b93db3447b4bbe532d5d4748f3a14a2b
RasSoul <mmagtc1101@gmail.com>
2025-03-15 20:23:55 -0500

    Mensaje de bienvenida

---
+++ guessing.js
@@ -0,0 +1,18 @@
+// Juego para adivinar número.
+
+// Seleccionar Aleatoriamente un número. 
+// Deliminar número de oportunidades que tendra el usuario para adivinar.
+// Una vez el usuario gane, el juego termina y el usuario ganará, de lo contrario el juego continuará. 
+
+/* REQUISITOS:
+    1. Mostrar mensaje de bienvenida una vez el juego inicie. 
+    2. Seleccionar aleatoriamente un número random entero de 1 a 100. 
+    3. Usuario seleccionar nivel de dificultad (fácil, medio, díficil).
+    4. Usuario debe poder introducir su opción. 
+    5. Si acierta el usuario, debe se mostrarsele un mensaje de felicitación con el número de intentos necesarios. 
+    6. Si no acierta el usuario, se le debe indicar en un mensaje si el número es mayor o menos que el indicado por el usuario. 
+    7. El juego debe terminar cuando el usuario adivina el número correcto o se le acaban las oportunidades. 
+*/
+
+// Mensaje de bienvenida. 
+console.log("Bienvenido al Juego de Adivinar Número!");
